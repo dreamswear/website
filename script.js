b66d30c0e0fcdc960cee3fd0c4a0a2d81f23ac3e
dreamswear <david.dicka@outlook.com>
2025-12-21 07:19:09 +0100

    site web

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-   //Configuration Supabase
-        const SUPABASE_URL = 'https://kfptsbpriihydidnfzhj.supabase.co';
-        const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmcHRzYnByaWloeWRpZG5memhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNjgxODIsImV4cCI6MjA4MTY0NDE4Mn0.R4AS9kj-o3Zw0OeOTAojMeZfjPtkOZiW0jM367Fmrkk';        
-        
-        const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
 document.addEventListener('DOMContentLoaded', () => {
 
     // ============================================
@@ -312,6 +306,11 @@ document.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     });
+   //Configuration Supabase
+        const SUPABASE_URL = 'https://kfptsbpriihydidnfzhj.supabase.co';
+        const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmcHRzYnByaWloeWRpZG5memhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNjgxODIsImV4cCI6MjA4MTY0NDE4Mn0.R4AS9kj-o3Zw0OeOTAojMeZfjPtkOZiW0jM367Fmrkk';        
+        
+        const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
     // ============================================
     // 8. EMPÊCHER LA SOUMISSION PAR DÉFAUT DES AUTRES FORMULAIRES
